c2e79cb80f38088fa4dfd8cd58f8a7bbc4814550
Tharun Adhi <tharunana@gmail.com>
2020-07-18 23:20:42 +0800

    Setup app Heroku app

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     //console.log(location)
-    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+    fetch('/weather?address='+ location).then((response) => {
     //console.log(response)
     response.json().then((data) => {
         //console.log(data)
--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const hbs = require('hbs')
 const app = express()
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-
+const port = process.env.PORT || 3000
 
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public'))
@@ -111,7 +111,7 @@ app.get('*',(req, res) => {
         errorMessage: 'Page not found'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log('Server is up on port' + port);
 })
 
